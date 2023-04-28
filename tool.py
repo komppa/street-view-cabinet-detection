67ad885e7c287ea829ebe08f55ca288c9218df89
komppa <posti@rantakangas.com>
2023-04-28 21:16:22 +0300

    add heading calculation for non 360 images

--- tool.py
+++ tool.py
@@ -6,6 +6,10 @@ import math
 This module contains functions for getting nearby panorama ids and positions
 for a given location (lat, lng).
 
+Also, heading can be calculated by using two positions (lat, lng). This can be
+used if we are downloading images that are not 360 degrees therefore we need
+to calculate the heading for each image to track road ditch where cabinet could be.
+
 The function get_nearby_panorama_ids() returns a dictionary with two keys:
     - panorama_ids: a set of panorama ids
     - panorama_positions: a list of dictionaries with lat and lng keys
@@ -53,6 +57,17 @@ def get_nearby_panorama_ids(api_key, lat, lng, radius=60, step=10):
         "panorama_ids": panorama_ids,
         "panorama_positions": panorama_positions
     }
+
+def calculate_heading(point1, point2):
+    lat1, lng1 = math.radians(point1[0]), math.radians(point1[1])
+    lat2, lng2 = math.radians(point2[0]), math.radians(point2[1])
+
+    dLng = lng2 - lng1
+    y = math.sin(dLng) * math.cos(lat2)
+    x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(dLng)
+    heading = math.degrees(math.atan2(y, x))
+
+    return (heading + 360) % 360
 
 
 # Usage example how to get nearby panorama ids and positions
